9d9d493caae4925cefb1763c13db7e98adaed5cb
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 20:01:24 -0300

    resolve exercicio treino 1, 2 e 3

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-2-array-e-for/script.js
@@ -0,0 +1,17 @@
+// Exercício 1 treino
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);*/
+
+// Exercício 2 treino
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);*/
+
+// Exercício 3 treino
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);*/
