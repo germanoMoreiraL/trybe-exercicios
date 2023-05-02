2c54cac905f285e55cbf3db477058b10a583fd9e
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 18:33:46 -0300

    resolve exercício 9 do dia

--- fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
@@ -112,11 +112,22 @@ switch (chessPiece.toLowerCase()) {
     }*/
 
 // Exercício 8
-let num1 = 5;
+/*let num1 = 5;
 let num2 = 10;
 let num3 = 15;
 
     if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }*/
+
+// Exercício 9
+let num1 = 46;
+let num2 = 30;
+let num3 = 25;
+
+    if (num1 % 2 !== 0 || num2 % 2 !== 0 || num3 % 2 !== 0) {
         console.log(true);
     } else {
         console.log(false);
