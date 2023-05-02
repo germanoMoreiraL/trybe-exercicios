76c7ef4a31a155316ac98cbd0dd81cd4f0188800
germanoMoreiraL <ge260203@gmail.com>
2023-05-01 23:53:40 -0300

    terceiro exercicio do dia

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script3.js
@@ -0,0 +1,9 @@
+let grade = 80;
+
+if (grade >= 80) {
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+} else if (grade < 80 && grade >= 60) {
+    console.log('Você está na nossa lista de espera.');
+} else {
+    console.log('Infelizmente, você reprovou.');
+}
