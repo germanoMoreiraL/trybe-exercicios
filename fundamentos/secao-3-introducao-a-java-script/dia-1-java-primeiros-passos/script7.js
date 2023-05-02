21db9916582efc4f0e5b5ac185bc03e264871895
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 19:19:02 -0300

    resolve exercício 11 do dia

--- fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
@@ -134,7 +134,7 @@ let num3 = 25;
     }*/
 
 // Exercício 10
-let cost = 10;
+/*let cost = 10;
 let value = 20;
 
     if (cost >= 0 && value >= 0) {
@@ -143,4 +143,36 @@ let value = 20;
         console.log(profit);
     } else {
         console.log('Error');
-    };
+    };*/
+
+// Exercício 11
+let aliquotINSS;
+let aliquotIR;
+
+let grossSalary = 3000.00;
+
+if (grossSalary <= 1556.94) {
+    aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+    aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+    aliquotINSS = grossSalary * 0.11;
+} else {
+    aliquotINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+    aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+    aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+    aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+    aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+    aliquotIR = (baseSalary * 0.275) - 869.36;
+}
+
+console.log('Salário líquido : ' + (baseSalary - aliquotIR));
