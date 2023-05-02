c883d47724332fce9e7a1b4db010585a4c8d2b88
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 15:27:59 -0300

    resolve exercicio 1, 2 e 3 do dia

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
@@ -0,0 +1,33 @@
+// Exercício 1
+let a = 15;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// Exercício 2
+let number1 = 20;
+let number2 = 30;
+
+    if (number1 > number2) {
+        console.log('Número 1 é maior que número 2');
+    } else {
+        console.log('Número 2 é maior que número 1');
+    }
+
+// Exercício 3
+let numberOne = 5;
+let numberTwo = 10;
+let numberThree = 15;
+
+    if (numberOne > numberTwo && numberOne > numberThree) {
+        console.log('numberOne é maior que numberTwo e numberThree');
+    } else if (numberTwo > numberOne && numberTwo > numberThree) {
+        console.log('numberTwo é maior que numberOne e numberThree');
+    } else {
+        console.log('numberThree é maior que numberOne e numberTwo');
+    }
+
