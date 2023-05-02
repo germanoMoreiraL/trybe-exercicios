f5a20b55bd4cc63ea10b055b36d91582ff7f2c77
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 18:24:29 -0300

    resolve exercício 8 do dia

--- fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script7.js
@@ -93,7 +93,7 @@ switch (chessPiece.toLowerCase()) {
 };*/
 
 //Exercício 7
-let grade = 90;
+/*let grade = 90;
 
     if (grade < 0 || grade > 100) {
         console.log('Erro, nota incorreta.');
@@ -109,5 +109,16 @@ let grade = 90;
         console.log('E');
     } else {
         console.log('F');
+    }*/
+
+// Exercício 8
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+
+    if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+        console.log(true);
+    } else {
+        console.log(false);
     }
     
