fd692d16fe4a5e155cc7066b56cf863cafa661c0
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 00:33:55 -0300

    sexto exercicio treino do dia

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-1-java-primeiros-passos/script6.js
@@ -0,0 +1,17 @@
+let result = 'lista';
+
+switch (result) {
+    case 'aprovada':
+        console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+        break;
+
+    case 'lista':
+        console.log('Você está na nossa lista de espera.');
+        break;
+
+    case 'reprovada':
+        console.log('Infelizmente, você reprovou.');
+
+    default:
+        console.log('Informação incorreta');
+}
