3c2a379ce3027a374827251e2c8151278c6cb309
germanoMoreiraL <ge260203@gmail.com>
2023-05-03 15:33:54 -0300

    exercicio pratica

--- fundamentos/secao-3-introducao-a-java-script/dia-2-array-e-for/script.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-2-array-e-for/script.js
@@ -31,3 +31,18 @@ for (let index = 0; index < groceryList.length; index += 1) {
 for (let index of names) {
     console.log(index);
 }*/
+
+//exercise.js
+// let fruits = [3, 4, 10, 1, 12];
+
+// let sum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+//     sum += fruits[index];
+// }
+
+// if (sum > 15) {
+//     console.log(sum);
+// } else {
+//     console.log('valor menor que 16');
+// }
