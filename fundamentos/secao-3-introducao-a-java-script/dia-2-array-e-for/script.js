768ded9cc044876a0b66208fde26db0f4c057892
germanoMoreiraL <ge260203@gmail.com>
2023-05-02 21:22:41 -0300

    resolve exercício 4 e 5 treino

--- fundamentos/secao-3-introducao-a-java-script/dia-2-array-e-for/script.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-2-array-e-for/script.js
@@ -15,3 +15,19 @@ console.log(indexOfPortfolio);*/
 menu.push('Contato')
 
 console.log(menu);*/
+
+
+// Exercício 4 treino
+/*let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}*/
+
+
+// Exercício 5 treino
+/*let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let index of names) {
+    console.log(index);
+}*/
