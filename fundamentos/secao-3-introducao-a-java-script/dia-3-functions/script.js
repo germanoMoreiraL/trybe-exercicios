cc42257a933f1da4e97e429f90c898f818b2fd4d
germanoMoreiraL <ge260203@gmail.com>
2023-05-04 12:14:20 -0300

    começo dia 3

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-3-functions/script.js
@@ -0,0 +1,25 @@
+// Exercicio pratica 1
+// let saldo = 300;
+
+// function addValue(valor) {
+//   return valor + saldo;
+// };
+
+// function deductValue(valor) {
+//   return saldo - valor;
+// };
+
+// function multValue(valor) {
+//   return valor * saldo;
+// };
+
+// function divideValue(valor) { 
+//   return saldo / valor;
+// };
+
+// console.log(addValue(150));
+// console.log(deductValue(75));
+// console.log(multValue(3));
+// console.log(divideValue(2));
+
+// Exercício pratica 2
