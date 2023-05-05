3f93586e64f6afcc5e88f30cd31eb22334855a2b
germanoMoreiraL <ge260203@gmail.com>
2023-05-05 19:55:05 -0300

    finalizando exercicios do dia

--- fundamentos/secao-3-introducao-a-java-script/dia-4-objects/script2.js
+++ fundamentos/secao-3-introducao-a-java-script/dia-4-objects/script2.js
@@ -124,3 +124,66 @@
 // );
 
 // console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos ');
+
+// Exercicio 9 pedido dos clientes
+// let order = {
+//   name: 'Rafael Andrade',
+//   phoneNumber: '11-98763-1416',
+//   address: {
+//     street: 'Rua das Flores',
+//     number: '389',
+//     apartment: '701',
+//   },
+//   order: {
+//     pizza: {
+//       marguerita: {
+//         amount: 1,
+//         price: 25,
+//       },
+//       pepperoni: {
+//         amount: 1,
+//         price: 20,
+//       },
+//     },
+//     drinks: {
+//       coke: {
+//         type: 'Coca-Cola Zero',
+//         price: 10,
+//         amount: 1,
+//       },
+//     },
+//     delivery: {
+//       deliveryPerson: 'Ana Silveira',
+//       price: 5,
+//     },
+//   },
+//   payment: {
+//     total: 60,
+//   },
+// };
+
+// //'Olá, Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'
+
+// function customerInfo(order) {
+//   let deliveryPerson = order.order.delivery.deliveryPerson;
+//   let deliveryTo = order.name;
+//   let phoneNumber = order.phoneNumber;
+//   let street = order.address.street;
+//   let houseNumber = order.address.number;
+//   let apNumber = order.address.apartment;
+
+//   console.log('Olá, ' + deliveryPerson + ', entrega para: ' + deliveryTo + ', Telefone: ' + phoneNumber + ', R. ' + street + ', N: ' + houseNumber + ', AP: ' + apNumber);
+// }
+
+// customerInfo(order);
+// //'Olá, Luiz Silva, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.
+// function orderModifier(order) {
+//   let newBuyer = order.name = 'Luiz Silva';
+//   let newPayment = order.payment.total = 50;
+//   let pizzas = Object.keys(order.order.pizza);
+//   let drinks = order.order.drinks.coke.type;
+
+//   console.log('Olá, ' + newBuyer + ', o valor total de seu pedido de ' + pizzas[0] + ', ' + pizzas[1] + ' e ' + drinks + ' é R$ ' + newPayment + ',00.');
+// }
+
+// orderModifier(order);
