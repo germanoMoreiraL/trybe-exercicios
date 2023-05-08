017b77dbedb3c04206ea3574776bfa5b78b9c82a
germanoMoreiraL <ge260203@gmail.com>
2023-05-08 15:46:42 -0300

    resolve exercicios pratica 1 a 4

---
+++ fundamentos/secao-3-introducao-a-java-script/dia-5-es6/script.js
@@ -0,0 +1,37 @@
+// Exercício pratica 1
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+ 
+// }
+// imprimeIdade()
+
+// Exercício 2
+// const pessoa = {
+//   nome: 'Henri',
+//   idade: 20
+// }
+
+// pessoa.nome = 'Luna'
+// pessoa.idade = 19
+
+// console.log('Nome:', pessoa.nome);
+// console.log('Idade:', pessoa.idade);
+
+// Exercício 3
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+// Exercício 4
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log(`Olá, ${name} ${lastName}!`); //'Olá' + ',' + name + ' ' + lastName + '!')
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);//'O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a,b)
