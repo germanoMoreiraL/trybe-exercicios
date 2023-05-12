36787465868612b2e29eb0676d68a5031ce0ab4e
germanoMoreiraL <ge260203@gmail.com>
2023-05-12 14:58:29 -0300

    finalizando exercicios pratica do dia

--- fundamentos/secao-4-dom-eventos/dia-2-trabalhando-elementos/script.js
+++ fundamentos/secao-4-dom-eventos/dia-2-trabalhando-elementos/script.js
@@ -18,19 +18,23 @@
 // const thirdChild2 = parent.lastElementChild.previousElementSibling;
 
 // Exercicio 2
-const pai = document.querySelector('#pai');
-let ondeVoceEstaIrmao = document.createElement('section');
-ondeVoceEstaIrmao.innerText = 'irmao';
-pai.appendChild(ondeVoceEstaIrmao);
+// const pai = document.querySelector('#pai');
+// let ondeVoceEstaIrmao = document.createElement('section');
+// ondeVoceEstaIrmao.innerText = 'irmao';
+// pai.appendChild(ondeVoceEstaIrmao);
 
-const ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
-let filhoOndeVoceEsta = document.createElement('section');
-filhoOndeVoceEsta.innerText = 'filho';
-ondeVoceEsta.appendChild(filhoOndeVoceEsta);
+// const ondeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+// let filhoOndeVoceEsta = document.createElement('section');
+// filhoOndeVoceEsta.innerText = 'filho';
+// ondeVoceEsta.appendChild(filhoOndeVoceEsta);
 
-const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
-let filhoPrimeiroFilhoDoFilho = document.createElement('section');
-filhoPrimeiroFilhoDoFilho.innerText = 'filho do primeiro filho';
-primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+// const primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+// let filhoPrimeiroFilhoDoFilho = document.createElement('section');
+// filhoPrimeiroFilhoDoFilho.innerText = 'filho do primeiro filho';
+// primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
 
-const terceiroFilho = filhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+// const terceiroFilho = filhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+
+// Exercicio 3
+const whereAreYou = document.querySelector('#where-are-you');
+whereAreYou.removeChild(whereAreYou.firstElementChild);
