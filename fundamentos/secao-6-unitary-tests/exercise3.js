fddf47cffe74e28cf348d4bef3fcae5e564ec39d
germanoMoreiraL <ge260203@gmail.com>
2023-05-29 18:27:09 -0300

    outra forma

--- fundamentos/secao-6-unitary-tests/exercise3.js
+++ fundamentos/secao-6-unitary-tests/exercise3.js
@@ -1,25 +1,40 @@
+// const studentRegister = (name, age) => {
+//   try {
+//     if (!name || !age) {
+//       throw new Error('Todas as informações são necessárias');
+//     }
+//     if (age < 18) {
+//       throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+//     }
+//     if (age >= 18) {
+//       return `${name}, seja bem-vindo(a) à AuTrybe!`;
+//     }
+//   } catch (error) {
+//     return error.message;
+//   }
+// }
+// console.log(studentRegister('Ana', 28));
+
+// outra forma
+const validateInfos = (name, age) => {
+  if (!name || !age) {
+    throw new Error('Todas as informações são necessárias');
+  }
+}
+
+const validAge = (age) => {
+  if (age < 18) {
+    throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+  }
+}
+
 const studentRegister = (name, age) => {
   try {
-    if (!name || !age) {
-      throw new Error('Todas as informações são necessárias');
-    }
-    if (age < 18) {
-      throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
-    }
-    if (age >= 18) {
-      return `${name}, seja bem-vindo(a) à AuTrybe!`;
-    }
+    validateInfos(name, age);
+    validAge(age);
+    return `${name}, seja bem-vindo(a) à AuTrybe!`;
   } catch (error) {
     return error.message;
   }
 }
 console.log(studentRegister('Ana', 28));
-
-// outra forma
-// const validateInfos = (name, age) => {
-
-// }
-
-// const studentRegister = (name, age) => {
-  
-// }
