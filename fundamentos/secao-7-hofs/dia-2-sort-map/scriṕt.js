faaebd22646027d444cb7bbe6b955398ffb0fae9
germanoMoreiraL <ge260203@gmail.com>
2023-06-06 18:32:15 -0300

    13123

--- fundamentos/secao-7-hofs/dia-2-sort-map/scriṕt.js
+++ fundamentos/secao-7-hofs/dia-2-sort-map/scriṕt.js
@@ -1,12 +1,21 @@
 // Para fixar 1
-const people = [
-  { name: 'Mateus', age: 18 },
-  { name: 'José', age: 16 },
-  { name: 'Ana', age: 23 },
-  { name: 'Cláudia', age: 20 },
-  { name: 'Bruna', age: 19 },
+// const people = [
+//   { name: 'Mateus', age: 18 },
+//   { name: 'José', age: 16 },
+//   { name: 'Ana', age: 23 },
+//   { name: 'Cláudia', age: 20 },
+//   { name: 'Bruna', age: 19 },
+// ];
+
+// // people.sort((a, b) => b.age - a.age);
+
+// console.log(people);
+
+const pessoas = [
+  { nome: 'Joana', idade: 37 },
+  { nome: 'Ana', idade: 25 },
+  { nome: 'João', idade: 32 },
+  { nome: 'Aline', idade: 28 },
 ];
 
-// people.sort((a, b) => b.age - a.age);
-
-console.log(people);
+pessoas.map((pessoa) => console.log(pessoa.nome));
