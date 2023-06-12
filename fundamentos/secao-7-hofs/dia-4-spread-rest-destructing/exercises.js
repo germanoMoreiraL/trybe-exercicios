87c3e51119adbc60914ad3abda6ee58d73329d86
germanoMoreiraL <ge260203@gmail.com>
2023-06-12 15:32:49 -0300

    exercicios do dia

---
+++ fundamentos/secao-7-hofs/dia-4-spread-rest-destructing/exercises.js
@@ -0,0 +1,116 @@
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+//   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
+// });
+
+// 2.
+// const sum = (...numbers) => numbers.reduce(((acc, val) => acc + val), 0);
+// console.log(sum(2,4,5,6,7));
+
+// 3.
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+// const alex = {
+//   name: 'Alex',
+//   age: 26,
+//   likes: ['fly fishing'],
+//   nationality: 'Australian',
+// };
+
+// const gunnar = {
+//   name: 'Gunnar',
+//   age: 30,
+//   likes: ['hiking', 'scuba diving', 'taking pictures'],
+//   nationality: 'Icelandic',
+// };
+
+// // complete a assinatura da função abaixo
+// const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// // <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+
+// // Retornos esperados:
+// console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// 4.
+// const people = [
+//   {
+//     name: 'Nicole',
+//     bornIn: 1992,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Harry',
+//     bornIn: 2008,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Toby',
+//     bornIn: 1901,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Frida',
+//     bornIn: 1960,
+//     nationality: 'Dannish',
+//   },
+//   {
+//     name: 'Fernando',
+//     bornIn: 2001,
+//     nationality: 'Brazilian',
+//   },
+//   // bornIn: nascido em
+// ];
+
+// const filterPeople = () => people
+//   .filter((person) => person.nationality === 'Australian' 
+//   && person.bornIn > 1900 && person.bornIn <= 2000);
+// console.log(filterPeople());
+
+// 5.
+// const ships = [
+//   {
+//     name: 'Titanic',
+//     length: 269.1,
+//     measurementUnit: 'meters',
+//   },
+//   {
+//     name: 'Queen Mary 2',
+//     length: 1132,
+//     measurementUnit: 'feet',
+//   },
+//   {
+//     name: 'Yamato',
+//     length: 256,
+//     measurementUnit: 'meters',
+//   },
+//   // measurementUnit: unidade de medida
+// ];
+
+// const shipLength = ({name, length, measurementUnit}) =>
+//  `${name} is ${length} ${measurementUnit} long`;
+
+// // retorno esperado
+// console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+// 6.
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { spring, summer, autumn, winter } = yearSeasons;
+
+const months = [...spring, ...summer, ...autumn, ...winter]
+console.log(months);
