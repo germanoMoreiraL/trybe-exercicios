a990198cb246c304468a325eb148fcd2a4158f2c
germanoMoreiraL <ge260203@gmail.com>
2023-06-13 20:12:59 -0300

    ok

--- fundamentos/secao-7-hofs/dia-5-array-destructuring/exercise.js
+++ fundamentos/secao-7-hofs/dia-5-array-destructuring/exercise.js
@@ -46,13 +46,13 @@
 // console.log(getLastName(student2));
 
 // 5.
-const moreStudents = [
-  'Chris', 
-  ['Ahmad', 'Antigoni'], 
-  ['Toby', 'Sam']
-];
+// const moreStudents = [
+//   'Chris', 
+//   ['Ahmad', 'Antigoni'], 
+//   ['Toby', 'Sam']
+// ];
 
-// Escreva seu código aqui
-const [student1, [student2, student3], [student4, student5]] = moreStudents;
+// // Escreva seu código aqui
+// const [student1, [student2, student3], [student4, student5]] = moreStudents;
 
-console.log(student1, student2, student3, student4, student5);
+// console.log(student1, student2, student3, student4, student5);
